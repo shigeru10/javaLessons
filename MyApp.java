bf6a54bd33b951b8a59806e84b3cd96d3b5e6a8b
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-09-25 17:57:35 +0900

    data type

--- MyApp.java
+++ MyApp.java
@@ -1,5 +1,20 @@
 public class MyApp {
   public static void main(String[] args) {
+
+    // 文字
+    char a = 'a';
+
+    // 整数 byte short int long
+    int x = 10;
+    long y = 5555555555555L;
+
+    // 浮動小数点数 float double
+    double d = 324.324
+    float f = 23.43F
+
+    // 真偽値
+    boolean flag = true;
+
     // 変数
     String msg = "Hello World";
 
